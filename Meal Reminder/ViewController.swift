5e065534ab42b3a711ebecd9f3081f9d2adab959
Yashvir <erpuchi@Yashs-MacBook-Air.local>
2019-02-03 12:28:22 +0530

    Initial Commit

---
+++ Meal Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meal Reminder
+//
+//  Created by Yash Walia on 03/02/19.
+//  Copyright © 2019 Yash Walia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Meal ReminderTests/Meal_ReminderTests.swift
@@ -0,0 +1,34 @@
+//
+//  Meal_ReminderTests.swift
+//  Meal ReminderTests
+//
+//  Created by Yash Walia on 03/02/19.
+//  Copyright © 2019 Yash Walia. All rights reserved.
+//
+
+import XCTest
+@testable import Meal_Reminder
+
+class Meal_ReminderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
